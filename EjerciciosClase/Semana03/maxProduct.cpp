5046ad5096972d0d0c928c0c12506bed432a9fae
GabrielPacco <gpaccohu@unsa.edu.pe>
2022-05-02 16:36:00 -0500

    Adding maxProduct

--- EjerciciosClase/Semana03/maxProduct.cpp
+++ EjerciciosClase/Semana03/maxProduct.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+int MaxProduct(const std :: vector<int>&numbers)
+{
 
+}
+
+int main(){
+    // Lectura de datos
+    int n;
+    std :: cin >>n;
+    std :: vector<int>numbers(n);
+    for(int i=0;i<n;++i){
+        std :: cin >> numbers[i];
+   }
+    // Salida de datos 
+    std :: cout << MaxProduct(numbers)<<"\n";
+
+    return 0;
+}
